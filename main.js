da42626f95112a22819d8d603a0e2fff24aa4a73
alexfont <alexfont321@gmail.com>
2018-07-18 00:13:41 -0500

    these terrible exercises

---
+++ main.js
@@ -0,0 +1,53 @@
+///Step 1
+
+
+// const getArticleHeader = document.querySelector(".article-header");
+
+// getArticleHeader.innerHTML = "Welcome to the DangerZone Blog";
+
+// console.log(getArticleHeader);
+
+
+
+///Step 2
+
+
+
+
+
+const articleHeaders = document.querySelector(".article-header");
+
+console.log(articleHeaders);
+
+
+const readFile = articleHeaders.classList;
+
+readFile.add("things");
+
+
+// articleHeaders.classList = "article-header important";
+
+console.log(articleHeaders);
+
+
+// const checkOut = document.querySelectorAll("#important");
+
+console.log(articleHeaders.classList);
+
+
+
+// console.log(articleHeaders.classList);
+
+// getAllArticleHeaders.classList = "important";
+
+// const getClass = articleHeaders.classList;
+
+// getClass.classList = "important";
+ 
+// console.log(getClass);
+
+// const getIDfromDom = document.querySelectorAll(".important");
+
+// let words = "Hello World"
+
+// getIDfromDom.innerHTML = words;
